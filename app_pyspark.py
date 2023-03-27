3ef7251e607f334c1134096df97fc0a66c677834
Tingyi Wanyan <tingyi@Tingyis-MBP.attlocal.net>
2023-03-27 13:39:33 -0400

    correction

--- app_pyspark.py
+++ app_pyspark.py
@@ -4,13 +4,14 @@ import pandas as pd
 
 spark = SparkSession.builder.master("local")\
 .appName("graph_server")\
+.config("spark.sql.warehouse.dir","/home/ubuntu")
 .enableHiveSupport()\
 .getOrCreate()
 
 
 columns = ["id", "source", "relation", "target"]
 
-data = [(1,"james",30,"M")]
+data = [(1,"james","where","Home")]
 
 triple_DF = spark.sparkContext.parallelize(data).toDF(columns)
 
